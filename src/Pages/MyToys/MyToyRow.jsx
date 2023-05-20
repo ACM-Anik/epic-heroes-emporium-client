193bcc828f23c63d16de3455a493536cb6682861
ACM-Anik <anikmojumder5252@gmail.com>
2023-05-21 00:44:56 +0600

    MyToys filter by email and design done

---
+++ src/Pages/MyToys/MyToyRow.jsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom";
+
+
+const MyToyRow = ({ toys, handleDelete }) => {
+    const {
+        _id,
+        picture,
+        toy_name,
+        seller_name,
+        seller_email,
+        category_name,
+        price, rating,
+        quantity,
+        // description
+    } = toys;
+    console.log(toys);
+
+
+
+    return (
+        <>
+            <tr>
+                <td>
+                    <div className="avatar">
+                        <div className="rounded w-24 h-24">
+                            {
+                                picture && <img src={picture} alt="service" />
+                            }
+                        </div>
+                    </div>
+                </td>
+                <td>{toy_name}</td>
+                {/* <td className="w-1/5">
+                    {
+                        description.length > 50
+                        ? 
+                        description.slice(0, 50) + '...'
+                        : 
+                        description
+                    }
+                </td> */}
+                <td>
+                    {category_name}
+                </td>
+                <td>{"$" + price}</td>
+                <td>{rating}</td>
+                <td>{quantity} pieces</td>
+                <td>{seller_name}</td>
+                <td>{seller_email}</td>
+                <td>
+                    <Link to={`/updateToy/${_id}`}>
+                        <button className="btn btn-sm text-white rounded-lg border-0 bg-[#000000] hover:bg-[#131111e8]">Update </button>
+                    </Link>
+                </td>
+                <td>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-sm text-white rounded-lg border-0 bg-[#FF0000] hover:bg-[#cb0707]">Delete </button>
+                </td>
+            </tr>
+        </>
+    );
+};
+
+export default MyToyRow;
---
+++ src/Pages/MyToys/MyToys.jsx
@@ -0,0 +1,72 @@
+import { useContext, useEffect, useState } from "react";
+import MyToyRow from "./MyToyRow";
+
+import { AuthContext } from "../../Providers/AuthProvider";
+
+
+const MyToys = () => {
+    const { user } = useContext(AuthContext);
+    const [myToys, setMyToys] = useState([]);
+
+    
+    useEffect(() => {
+        fetch(`https://epic-heroes-emporium.vercel.app/myToys/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setMyToys(data);
+            })
+    }, [user])
+
+
+
+    const handleDelete = (_id) => {
+            console.log(_id);
+    }
+
+    return (
+        <>
+            {
+                myToys ?
+                    <div className="my-16">
+                        <div className="my-10">
+                            <h2 className="text-5xl font-bold text-center">My <span className="text-[#FF0000]">TOYS</span></h2>
+                            <p className="text-base font-semibold text-center w-2/4 mx-auto my-4">All the toys our sellers added for sell here we have. Our products have the best quality in the market. We filter the best product for the customers. </p>
+                        </div>
+                        <div className="overflow-x-auto w-full">
+                            <table className="table w-full bg-slate-100">
+                                <thead>
+                                    <tr>
+                                        <th>Image</th>
+                                        <th>Toy Name</th>
+                                        {/* <th>Details</th> */}
+                                        <th>Category</th>
+                                        <th>price</th>
+                                        <th>Rating</th>
+                                        <th>Available Quantity</th>
+                                        <th>Seller</th>
+                                        <th>Seller Email</th>
+                                        <th>Update</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        myToys.map(toys => <MyToyRow
+                                            key={toys._id}
+                                            toys={toys}
+                                            handleDelete={handleDelete}
+                                        ></MyToyRow>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    :
+                    <h2 className="text-center text-5xl font-bold">You didn't added any action toy product yet!</h2>
+            }
+        </>
+    );
+};
+
+export default MyToys;
