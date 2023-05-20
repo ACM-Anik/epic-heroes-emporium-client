b875ac2c1c6ac7117b9149c73cc077681ceb1186
ACM-Anik <anikmojumder5252@gmail.com>
2023-05-20 07:02:57 +0600

    blog page done

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -32,6 +32,7 @@ const Blog = () => {
                     It's important to follow security best practices and consider the specific requirements of your application when implementing token storage and handling mechanisms.
                 </p>
             </div>
+
             <div className="my-6 py-10 px-20 shadow-xl shadow-red-100 ">
                 <h2 className="text-2xl font-bold font-serif mb-6">2. Compare SQL and NoSQL databases?</h2>
                 <p className="text-lg font-semibold font-mono">
@@ -60,6 +61,27 @@ const Blog = () => {
                     <br />
                     <br />
                     Both SQL and NoSQL databases have their strengths and weaknesses, and the choice between them depends on the specific requirements of your application. In some cases, a hybrid approach that combines both types of databases may be appropriate to leverage the advantages of each.
+                </p>
+            </div>
+
+            <div className="my-6 py-10 px-20 shadow-xl shadow-red-100 ">
+                <h2 className="text-2xl font-bold font-serif mb-6">3. What is express js? What is Nest JS?</h2>
+                <p className="text-lg font-semibold font-mono">
+                    <span className="text-xl font-bold">Express.js: </span>
+                    <br />
+                    Express.js is a popular web application framework for Node.js. It provides a minimalistic and flexible set of features for building web applications and APIs. Express.js allows developers to handle HTTP requests, define routes, handle middleware functions, and manage views for rendering dynamic content. It is known for its simplicity, lightweight nature, and extensive middleware ecosystem, which allows developers to easily integrate additional functionality into their applications.
+                    <br />
+                    <br />
+                    <span className="text-xl font-bold">Nest.js:</span>
+                    <br />
+                    Nest.js is a progressive Node.js framework for building scalable and maintainable server-side applications. It is built on top of Express.js and leverages TypeScript, bringing features from object-oriented programming and functional programming paradigms to Node.js development. Nest.js provides a modular architecture, dependency injection, powerful routing system, and built-in support for various features like validation, caching, and authentication. It promotes the use of decorators, modules, and providers to create well-structured and testable code. Nest.js is suitable for building complex enterprise applications with a focus on scalability, maintainability, and developer productivity.
+                </p>
+            </div>
+
+            <div className="my-6 py-10 px-20 shadow-xl shadow-red-100 ">
+                <h2 className="text-2xl font-bold font-serif mb-6">4. What is MongoDB aggregate and how does it work ?</h2>
+                <p className="text-lg font-semibold font-mono">
+                    The MongoDB aggregate method is used for advanced data processing and analysis in MongoDB. It works by applying a series of stages to a collection of documents. Each stage performs a specific operation on the data, such as filtering, grouping, sorting, or joining. The stages are executed in order, with each stage receiving input from the previous stage and passing the transformed data to the next stage. The aggregate method allows you to perform complex data operations within the database, reducing the need to transfer large amounts of data to the application layer.
                 </p>
             </div>
         </div>
